ae39e36bc7a92dfcb4514e8ce21a70b9d2cbd2af
Luke Davis <luke@loopapps.com>
2023-04-27 14:06:51 -0400

    Updates the download url

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FireworkVideoIVSSupport",
-            url: "https://github.com/loopsocial/firework_ios_sdk_ivs_support/releases/download/v0.4.0/FireworkVideoIVSSupport-v0.4.0.xcframework.zip",
+            url: "https://github.com/loopsocial/firework_ios_sdk_ivs_support/releases/download/v0.5.0/FireworkVideoIVSSupport-v0.5.0.xcframework.zip",
             checksum: "477568277138b338a6dbaf2bf0ebd3961ddce138a844279724cf0bf8ce004ff7"
         ),
         .target(
