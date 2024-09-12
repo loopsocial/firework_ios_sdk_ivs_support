5e836a02ac5930b724b5344343a98d753bb5c2e6
Firework <loopops@users.noreply.github.com>
2024-09-12 07:47:17 +0000

    Update SDK support library to v0.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,11 +3,17 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.7.0]
+
+### Changed
+
+- Upgrade `AmazonIVSPlayer` to V1.32.0.
+
 ## [0.6.0]
 
 ### Changed
 
-- Upgrade AmazonIVSPlayer to fix the PiP crash on specific devices.
+- Upgrade `AmazonIVSPlayer` to fix the PiP crash on specific devices.
 
 ## [0.5.0]
 
--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FireworkVideoIVSSupport",
-            url: "https://github.com/loopsocial/firework_ios_sdk_ivs_support/releases/download/v0.6.0/FireworkVideoIVSSupport-v0.6.0.xcframework.zip",
-            checksum: "22dcaffd6f4acf169a8f90a3e9171693555eef01ba557365135cab2ae4050910"
+            url: "https://github.com/loopsocial/firework_ios_sdk_ivs_support/releases/download/v0.7.0/FireworkVideoIVSSupport-v0.7.0.xcframework.zip",
+            checksum: "8f7ae47ed8448f881fa21e54570e53700378788dc8203cb30918d48c61b79829"
         ),
         .target(
             name: "FireworkVideoIVSSupportDependencies",
