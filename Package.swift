2e305245567df3405352ee0da97934bcda9ed948
Firework <loopops@users.noreply.github.com>
2026-06-12 06:16:43 +0000

    Update SDK support library to v0.9.8-beta.3

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FireworkVideoIVSSupport",
-            url: "https://github.com/loopsocial/firework_ios_sdk_ivs_support/releases/download/v0.9.8-beta.2/FireworkVideoIVSSupport-v0.9.8-beta.2.xcframework.zip",
-            checksum: "4ce1f9c9f60899bb2fd11e3cc2a9848b1abb4426df3b626724523acf956e9382"
+            url: "https://github.com/loopsocial/firework_ios_sdk_ivs_support/releases/download/v0.9.8-beta.3/FireworkVideoIVSSupport-v0.9.8-beta.3.xcframework.zip",
+            checksum: "fa4796b40e174aa5e987f3d96ac229d650ac0d1086a8a3fe39a75228d0c47910"
         ),
         .target(
             name: "FireworkVideoIVSSupportDependencies",
